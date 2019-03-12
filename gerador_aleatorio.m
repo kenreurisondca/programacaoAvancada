cf224f887b75b9b0e404feebc2c247a08788923b
kenreurison <kenreurison@dca.ufrn.br>
2019-03-12 00:00:07 -0300

    Sequencias para serem ordenadas.

--- gerador_aleatorio.m
+++ gerador_aleatorio.m
@@ -1,7 +1,16 @@
-n = 1000;
+n = 100000;
 digits 4;
 x = round(n*(rand(n,1)));
 x = vpa(x(1:n/10));
-fid = fopen('sequencia.txt','w');
-fprintf(fid,'%hd ',x);
+
+fid = fopen('sequencia01.txt','w');
+fprintf(fid,'%hd ',vpa(x(1:n/100)));
+fclose(fid);
+
+fid = fopen('sequencia02.txt','w');
+fprintf(fid,'%hd ',vpa(x(1:n/50)));
+fclose(fid);
+
+fid = fopen('sequencia03.txt','w');
+fprintf(fid,'%hd ',vpa(x(1:n/10)));
 fclose(fid);
