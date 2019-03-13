76a9de3faa5327e35d54b4db299f78b31fba078a
kenreurison <kenreurison@dca.ufrn.br>
2019-03-12 23:30:16 -0300

    Sequencias sem repetição e organizados para copia e cola na linha de comando do Prolog.

--- gerador_aleatorio.m
+++ gerador_aleatorio.m
@@ -1,16 +1,27 @@
-n = 100000;
+n = 10000;
 digits 4;
 x = round(n*(rand(n,1)));
-x = vpa(x(1:n/10));
+x = unique(vpa(x));
+x = x(randperm(length(x)));
+
+x1 = x(1:end*0.01);
+x2 = x(1:end*0.1);
+x3 = x(1:end*0.5);
 
 fid = fopen('sequencia01.txt','w');
-fprintf(fid,'%hd ',vpa(x(1:n/100)));
+fprintf(fid, '[');
+fprintf(fid,'%hd, ',vpa(x1(1:end-1)));
+fprintf(fid,'%hd]',vpa(x1(end)));
 fclose(fid);
 
 fid = fopen('sequencia02.txt','w');
-fprintf(fid,'%hd ',vpa(x(1:n/50)));
+fprintf(fid, '[');
+fprintf(fid,'%hd, ',vpa(x2(1:end-1)));
+fprintf(fid,'%hd]',vpa(x2(end)));
 fclose(fid);
 
 fid = fopen('sequencia03.txt','w');
-fprintf(fid,'%hd ',vpa(x(1:n/10)));
+fprintf(fid, '[');
+fprintf(fid,'%hd, ',vpa(x3(1:end-1)));
+fprintf(fid,'%hd]',vpa(x3(end)));
 fclose(fid);
